a58509bb8f02d9a82ef9983e0ddbf15dba0b6cd9
Kevin <mybaseball52@gmail.com>
2017-12-08 14:30:10 +0800

    add game.java

---
+++ src/main/java/homework/Game.java
@@ -0,0 +1,14 @@
+package homework;
+
+public class Game {
+    public static enum Role{
+        NONE,
+        AI,
+        PLAYER
+    }
+
+    public enum DirectionOfMovement{
+        FOREWARD,
+        BACKWARD
+    }
+}
